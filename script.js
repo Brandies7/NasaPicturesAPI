8e71c05ac400e1bdc99ab4057dd4fc7e7f9abbe0
Rudy Brandies <adanb82@gmail.com>
2023-11-01 19:30:22 -0500

    connected to nasa api

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const count = 10;
+const apiKey = 'DEMO_KEY'
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+
+async function getNasaPictures() {
+    try {
+        const response = await fetch(apiUrl);
+        resultsArray = await response.json();
+
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+getNasaPictures();
